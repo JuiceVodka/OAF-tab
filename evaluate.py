5e1c091fed5928acb4d8028eef648c7585efd916
Igor Nikolaj Sok <nikolajsok@gmail.com>
2023-02-19 12:10:26 +0100

    initial commit

---
+++ evaluate.py
@@ -0,0 +1,213 @@
+import argparse
+import os
+import sys
+from collections import defaultdict
+
+import numpy as np
+from mir_eval.multipitch import evaluate as evaluate_frames
+from mir_eval.transcription import precision_recall_f1_overlap as evaluate_notes
+from mir_eval.transcription_velocity import precision_recall_f1_overlap as evaluate_notes_with_velocity
+from mir_eval.util import midi_to_hz
+from scipy.stats import hmean
+from tqdm import tqdm
+
+import onsets_and_frames.dataset as dataset_module
+from onsets_and_frames import *
+
+eps = sys.float_info.epsilon
+
+#tab pitch metrics
+
+def tabToPitch(tab):
+    stringsMidi = [40, 45, 50, 55, 59, 64]
+    pitchVector = np.zeros(44)
+    for string in range(tab.shape[0]):
+        fret = tab[string, :]
+        played = np.argmax(fret, -1)
+        if(played > 0):
+            pitch = played + stringsMidi[string] - 41  # to move down by pitch of lower e string + 1 for string not played class
+            pitchVector[pitch] = 1
+    return pitchVector
+
+def tabToVec(tab):
+    fretBoard = np.zeros((6, 20))
+    for string in range(tab.shape[0]):
+        stringVec = tab[string, :]
+        played = np.argmax(stringVec, -1)
+        if(played > 0):
+            fret = played - 1
+            fretBoard[string][fret] = 1
+    return fretBoard
+
+def pitchPrecision(prediction, truth):
+    pitchPred = np.array(list(map(tabToPitch, prediction)))
+    pitchTruth = np.array(list(map(tabToPitch, truth)))
+    precision = np.sum(np.multiply(pitchPred, pitchTruth).flatten()) / np.sum(pitchPred.flatten())
+    return precision
+
+
+def pitchRecall(prediction, truth):
+    pitchPred = np.array(list(map(tabToPitch, prediction)))
+    pitchTruth = np.array(list(map(tabToPitch, truth)))
+    recall = np.sum(np.multiply(pitchPred, pitchTruth).flatten()) / np.sum(pitchTruth.flatten())
+    return recall
+
+def tabPrecision(prediction, truth):
+    tabPred = np.array(list(map(tabToVec, prediction)))
+    tabTruth = np.array(list(map(tabToVec, truth)))
+    precision = np.sum(np.multiply(tabPred, tabTruth).flatten()) / np.sum(tabPred.flatten())
+    return precision
+
+
+def tabRecall(prediction, truth):
+    tabPred = np.array(list(map(tabToVec, prediction)))
+    tabTruth = np.array(list(map(tabToVec, truth)))
+    recall = np.sum(np.multiply(tabPred, tabTruth).flatten()) / np.sum(tabTruth.flatten())
+    return recall
+
+
+
+def fMeasure(prediction, truth, isTab):
+    precision = tabPrecision(prediction, truth) if isTab else pitchPrecision(prediction, truth)
+    recall = tabRecall(prediction, truth) if isTab else pitchRecall(prediction, truth)
+    f = (2 * precision * recall) / (precision + recall)
+    return f
+
+
+
+def evaluate(data, model, onset_threshold=0.5, frame_threshold=0.5, save_path=None):
+    metrics = defaultdict(list)
+
+    for label in data:
+        #print(label)
+        pred, losses = model.run_on_batch(label)
+
+        for key, loss in losses.items():
+            metrics[key].append(loss.item())
+
+        for key, value in pred.items():
+            value.squeeze_(0).relu_()
+
+        p_ref, i_ref = extract_notes(label['onset'], label['frame'])
+        p_est, i_est = extract_notes(pred['onset'], pred['frame'], onset_threshold, frame_threshold)
+
+        t_ref, f_ref = notes_to_frames(p_ref, i_ref, label['frame'].shape)
+        t_est, f_est = notes_to_frames(p_est, i_est, pred['frame'].shape)
+
+        scaling = HOP_LENGTH / SAMPLE_RATE
+
+        i_ref = (i_ref * scaling).reshape(-1, 2)
+        p_ref = np.array([midi_to_hz(MIN_MIDI + midi) for midi in p_ref])
+        i_est = (i_est * scaling).reshape(-1, 2)
+        p_est = np.array([midi_to_hz(MIN_MIDI + midi) for midi in p_est])
+
+        t_ref = t_ref.astype(np.float64) * scaling
+        f_ref = [np.array([midi_to_hz(MIN_MIDI + midi) for midi in freqs]) for freqs in f_ref]
+        t_est = t_est.astype(np.float64) * scaling
+        f_est = [np.array([midi_to_hz(MIN_MIDI + midi) for midi in freqs]) for freqs in f_est]
+
+        p, r, f, o = evaluate_notes(i_ref, p_ref, i_est, p_est, offset_ratio=None)
+        metrics['metric/note/precision'].append(p)
+        metrics['metric/note/recall'].append(r)
+        metrics['metric/note/f1'].append(f)
+        metrics['metric/note/overlap'].append(o)
+
+        p, r, f, o = evaluate_notes(i_ref, p_ref, i_est, p_est)
+        metrics['metric/note-with-offsets/precision'].append(p)
+        metrics['metric/note-with-offsets/recall'].append(r)
+        metrics['metric/note-with-offsets/f1'].append(f)
+        metrics['metric/note-with-offsets/overlap'].append(o)
+
+        """p, r, f, o = evaluate_notes_with_velocity(i_ref, p_ref, v_ref, i_est, p_est, v_est,
+                                                  offset_ratio=None, velocity_tolerance=0.1)
+        metrics['metric/note-with-velocity/precision'].append(p)
+        metrics['metric/note-with-velocity/recall'].append(r)
+        metrics['metric/note-with-velocity/f1'].append(f)
+        metrics['metric/note-with-velocity/overlap'].append(o)
+
+        p, r, f, o = evaluate_notes_with_velocity(i_ref, p_ref, v_ref, i_est, p_est, v_est, velocity_tolerance=0.1)
+        metrics['metric/note-with-offsets-and-velocity/precision'].append(p)
+        metrics['metric/note-with-offsets-and-velocity/recall'].append(r)
+        metrics['metric/note-with-offsets-and-velocity/f1'].append(f)
+        metrics['metric/note-with-offsets-and-velocity/overlap'].append(o)"""
+
+        frame_metrics = evaluate_frames(t_ref, f_ref, t_est, f_est)
+        metrics['metric/frame/f1'].append(hmean([frame_metrics['Precision'] + eps, frame_metrics['Recall'] + eps]) - eps)
+
+        for key, loss in frame_metrics.items():
+            metrics['metric/frame/' + key.lower().replace(' ', '_')].append(loss)
+
+        if save_path is not None:
+            os.makedirs(save_path, exist_ok=True)
+            label_path = os.path.join(save_path, os.path.basename(label['path']) + '.label.png')
+            save_pianoroll(label_path, label['onset'], label['frame'])
+            pred_path = os.path.join(save_path, os.path.basename(label['path']) + '.pred.png')
+            save_pianoroll(pred_path, pred['onset'], pred['frame'])
+            midi_path = os.path.join(save_path, os.path.basename(label['path']) + '.pred.mid')
+            #save_midi(midi_path, p_est, i_est, v_est)
+            
+            
+        predTab = pred['tab'].cpu().numpy()
+        labelTab = label['tab'].cpu().numpy()
+        
+        metrics['metric/tab/pitch-precision'].append(pitchPrecision(predTab, labelTab))
+        metrics['metric/tab/pitch-recall'].append(pitchRecall(predTab, labelTab))
+        metrics['metric/tab/pitch-fScore'].append(fMeasure(predTab, labelTab, False))
+        metrics['metric/tab/tab-precision'].append(tabPrecision(predTab, labelTab))
+        metrics['metric/tab/tab-recall'].append(tabRecall(predTab, labelTab))
+        metrics['metric/tab/tab-fScore'].append(fMeasure(predTab, labelTab, True))
+        
+        
+        predTabComb = pred['comb_tab'].cpu().numpy()
+        
+        metrics['metric/tab-comb/pitch-precision'].append(pitchPrecision(predTabComb, labelTab))
+        metrics['metric/tab-comb/pitch-recall'].append(pitchRecall(predTabComb, labelTab))
+        metrics['metric/tab-comb/pitch-fScore'].append(fMeasure(predTabComb, labelTab, False))
+        metrics['metric/tab-comn/tab-precision'].append(tabPrecision(predTabComb, labelTab))
+        metrics['metric/tab-comb/tab-recall'].append(tabRecall(predTabComb, labelTab))
+        metrics['metric/tab-comb/tab-fScore'].append(fMeasure(predTabComb, labelTab, True))
+        
+        """print(f"Tab pitch precission: {pitchPrecision(predTab, labelTab)}")
+        print(f"Tab pitch recall: {pitchRecall(predTab, labelTab)}")
+        print(f"Tab pitch f-score: {fMeasure(predTab, labelTab, False)}")
+        
+        print(f"Tab precision: {tabPrecision(predTab, labelTab)}")
+        print(f"Tab recall: {tabRecall(predTab, labelTab)}")
+        print(f"Tab f-score: {fMeasure(predTab, labelTab, True)}")"""
+        
+
+    return metrics
+
+
+def evaluate_file(model_file, dataset, dataset_group, sequence_length, save_path,
+                  onset_threshold, frame_threshold, device):
+    dataset_class = getattr(dataset_module, dataset)
+    kwargs = {'sequence_length': sequence_length, 'device': device}
+    if dataset_group is not None:
+        kwargs['groups'] = [dataset_group]
+    dataset = dataset_class(**kwargs)
+
+    model = torch.load(model_file, map_location=device).eval()
+    summary(model)
+
+    metrics = evaluate(tqdm(dataset), model, onset_threshold, frame_threshold, save_path)
+
+    for key, values in metrics.items():
+        if key.startswith('metric/'):
+            _, category, name = key.split('/')
+            print(f'{category:>32} {name:25}: {np.mean(values):.3f} ± {np.std(values):.3f}')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model_file', type=str)
+    parser.add_argument('dataset', nargs='?', default='GuitarSet')
+    parser.add_argument('dataset_group', nargs='?', default=None)
+    parser.add_argument('--save-path', default=None)
+    parser.add_argument('--sequence-length', default=None, type=int)
+    parser.add_argument('--onset-threshold', default=0.5, type=float)
+    parser.add_argument('--frame-threshold', default=0.5, type=float)
+    parser.add_argument('--device', default='cuda' if torch.cuda.is_available() else 'cpu')
+
+    with torch.no_grad():
+        evaluate_file(**vars(parser.parse_args()))
---
+++ onsets_and_frames/transcriber.py
@@ -0,0 +1,234 @@
+"""
+A rough translation of Magenta's Onsets and Frames implementation [1].
+
+    [1] https://github.com/tensorflow/magenta/blob/master/magenta/models/onsets_frames_transcription/model.py
+"""
+
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+from .lstm import BiLSTM
+from .mel import melspectrogram
+
+class StringSoftmax(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+    def forward(self, x):
+        #frst_dim = (x.shape[0]*x.shape[1]*x.shape[2])//(6*21)
+        #print(x.shape)
+        x = torch.reshape(x, (x.shape[0] , x.shape[1], 6, 21))#a ta reshape naredi pravilno po vrsticah ali p stolpcih?
+        
+        """for i in range(6):
+            x[:, :, i, :] = torch.softmax(x[:, :, i, :], dim=3)"""
+        x = torch.softmax(x, dim=3)
+        
+        return x
+    
+
+
+class ConvStack(nn.Module):
+    def __init__(self, input_features, output_features):
+        super().__init__()
+
+        # input is batch_size * 1 channel * frames * input_features
+        self.cnn = nn.Sequential(
+            # layer 0
+            nn.Conv2d(1, output_features // 16, (3, 3), padding=1),
+            nn.BatchNorm2d(output_features // 16),
+            nn.ReLU(),
+            # layer 1
+            nn.Conv2d(output_features // 16, output_features // 16, (3, 3), padding=1),
+            nn.BatchNorm2d(output_features // 16),
+            nn.ReLU(),
+            # layer 2
+            nn.MaxPool2d((1, 2)),
+            nn.Dropout(0.25),
+            nn.Conv2d(output_features // 16, output_features // 8, (3, 3), padding=1),
+            nn.BatchNorm2d(output_features // 8),
+            nn.ReLU(),
+            # layer 3
+            nn.MaxPool2d((1, 2)),
+            nn.Dropout(0.25),
+        )
+        self.fc = nn.Sequential(
+            nn.Linear((output_features // 8) * (input_features // 4), output_features),
+            nn.Dropout(0.5)
+        )
+
+    def forward(self, mel):
+        x = mel.view(mel.size(0), 1, mel.size(1), mel.size(2))
+        x = self.cnn(x)
+        x = x.transpose(1, 2).flatten(-2)
+        x = self.fc(x)
+        return x
+
+
+class OnsetsAndFrames(nn.Module):
+    def __init__(self, input_features, output_features, model_complexity=48):
+        super().__init__()
+
+        model_size = model_complexity * 16
+        sequence_model = lambda input_size, output_size: BiLSTM(input_size, output_size // 2)
+
+        self.onset_stack = nn.Sequential(
+            ConvStack(input_features, model_size),
+            sequence_model(model_size, model_size),
+            nn.Linear(model_size, output_features),
+            nn.Sigmoid()
+        )
+        self.offset_stack = nn.Sequential(
+            ConvStack(input_features, model_size),
+            sequence_model(model_size, model_size),
+            nn.Linear(model_size, output_features),
+            nn.Sigmoid()
+        )
+        self.frame_stack = nn.Sequential(
+            ConvStack(input_features, model_size),
+            nn.Linear(model_size, output_features),
+            nn.Sigmoid()
+        )
+        self.combined_stack = nn.Sequential(
+            sequence_model(output_features * 3, model_size),
+            nn.Linear(model_size, output_features),
+            nn.Sigmoid()
+        )
+        self.tab_stack = nn.Sequential(
+            ConvStack(input_features, model_size),
+            sequence_model(model_size, model_size),
+            nn.Linear(model_size, 126),
+            #nn.Linear(output_features, 128)
+            #nn.Linear(128, 126)
+            StringSoftmax()
+        )
+        self.combined_tab_stack = nn.Sequential(
+            sequence_model(output_features * 3, model_size),
+            nn.Linear(model_size, 126),
+            StringSoftmax()
+        )
+        """self.velocity_stack = nn.Sequential(
+            ConvStack(input_features, model_size),
+            nn.Linear(model_size, output_features)
+        )"""
+
+    def forward(self, mel):
+        onset_pred = self.onset_stack(mel)
+        #print(onset_pred.shape)
+        offset_pred = self.offset_stack(mel)
+        #print(offset_pred.shape)
+        activation_pred = self.frame_stack(mel)
+        combined_pred = torch.cat([onset_pred.detach(), offset_pred.detach(), activation_pred], dim=-1)
+        frame_pred = self.combined_stack(combined_pred)
+        tab_pred = self.tab_stack(mel)
+        combined_tab_pred = self.combined_tab_stack(combined_pred)
+        #velocity_pred = self.velocity_stack(mel)
+        return onset_pred, offset_pred, activation_pred, frame_pred, tab_pred, combined_tab_pred#, velocity_pred
+
+    def run_on_batch(self, batch):
+        audio_label = batch['audio']
+        onset_label = batch['onset']
+        offset_label = batch['offset']
+        frame_label = batch['frame']
+        tab_label = batch['tab']
+        #velocity_label = batch['velocity']
+
+        mel = melspectrogram(audio_label.reshape(-1, audio_label.shape[-1])[:, :-1]).transpose(-1, -2)
+        onset_pred, offset_pred, _, frame_pred, tab_pred, combined_tab_pred = self(mel)
+        
+        #tab_pred = tab_pred.reshape(*tab_label.shape)
+        #print(onset_label.shape)
+        #print(tab_pred.shape)
+        #print(tab_label.shape)
+        
+        #tab_pred = self.softmax_by_string(tab_pred)
+
+        predictions = {
+            'onset': onset_pred.reshape(*onset_label.shape),
+            'offset': offset_pred.reshape(*offset_label.shape),
+            'frame': frame_pred.reshape(*frame_label.shape),
+            'tab' : tab_pred.reshape(*tab_label.shape),
+            'comb_tab' : combined_tab_pred.reshape(*tab_label.shape),
+            'tab_binary' : tab_pred,
+            'comb_tab_binary' : combined_tab_pred
+        }
+
+        losses = {
+            'loss/onset': F.binary_cross_entropy(predictions['onset'], onset_label),
+            'loss/offset': F.binary_cross_entropy(predictions['offset'], offset_label),
+            'loss/frame': F.binary_cross_entropy(predictions['frame'], frame_label),
+            'loss/tab' : self.tab_loss(predictions['tab'], tab_label),
+            'loss/comb_tab' : self.categorical_cross_entropy_by_string(predictions['comb_tab'], tab_label),
+            'loss/tab_binary': F.binary_cross_entropy(predictions['tab_binary'].view(-1, predictions['tab_binary'].size(-1)), tab_label.view(-1, tab_label.size(-1)).float()),
+            'loss/comb_tab_binary': F.binary_cross_entropy(predictions['comb_tab_binary'].view(-1, predictions['comb_tab_binary'].size(-1)), tab_label.view(-1, tab_label.size(-1)).float())
+        }
+
+        return predictions, losses
+
+    def velocity_loss(self, velocity_pred, velocity_label, onset_label):
+        denominator = onset_label.sum()
+        if denominator.item() == 0:
+            return denominator
+        else:
+            return (onset_label * (velocity_label - velocity_pred) ** 2).sum() / denominator
+
+    
+    def tab_loss(self, tab_pred, tab_label):
+        #print(tab_pred.shape)
+        #print(tab_label.shape)
+        #print(tab_label[:, :, 0, :].argmax(dim=2).shape)
+        #loss = 0
+        #for i in range(6):
+        #    loss += F.cross_entropy(tab_pred[:,:,i,:], tab_label[:,:,i,:].argmax(dim=2), reduction='mean')
+        target = tab_label.view(-1, tab_label.size(-1))
+        output = tab_pred.view(-1, tab_pred.size(-1))
+        loss = F.cross_entropy(output, target.argmax(dim=-1), reduction="mean")
+        return loss
+    
+    
+    def categorical_cross_entropy_by_string(self, tab_pred, tab_label):
+        #print(tab_pred.shape)
+        if(len(tab_pred.shape) == 4):
+            batch_size, seq_len, num_strings, num_classes = tab_pred.shape
+            
+            # Reshape to [batch_size * seq_len * num_strings, num_classes]
+            tab_pred_flat = tab_pred.view(-1, num_classes)
+            tab_label_flat = tab_label.view(-1, num_classes)
+            
+            # Compute categorical cross-entropy for each string
+            losses = []
+            for i in range(num_strings):
+                start_idx = i * batch_size * seq_len
+                end_idx = (i+1) * batch_size * seq_len
+                string_pred = tab_pred_flat[start_idx:end_idx]
+                string_label = tab_label_flat[start_idx:end_idx].argmax(dim=1)
+                loss = F.cross_entropy(string_pred, string_label, reduction='mean')
+                losses.append(loss)
+            
+            # Return sum of losses for all strings
+            return sum(losses)
+        else:
+            seq_len, num_strings, num_classes = tab_pred.shape
+            tab_pred_flat = tab_pred.view(-1, num_classes)
+            tab_label_flat = tab_label.view(-1, num_classes)
+            losses = []
+            for i in range(num_strings):
+                start_idx = i*seq_len
+                end_idx = (i+1)*seq_len
+                string_pred = tab_pred_flat[start_idx:end_idx]
+                string_label = tab_label_flat[start_idx:end_idx].argmax(dim=1)
+                loss = F.cross_entropy(string_pred, string_label, reduction="mean")
+                losses.append(loss)
+            return sum(losses)
+                
+    
+    def softmax_by_string(self, x):
+        #frst_dim = (x.shape[0]*x.shape[1]*x.shape[2])//(6*21)
+        #x = torch.reshape(x, (frst_dim, 6, 21))
+        string_sm = []
+        for i in range(6):
+            string_sm.append(torch.softmax(x[:, i, :], dim=1))
+        return torch.cat(string_sm, dim=1)
+            
+
+        
